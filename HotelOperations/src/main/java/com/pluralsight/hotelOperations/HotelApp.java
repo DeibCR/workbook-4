35ac92c701308ae9cf8370fdc629408ccc93f359
Deibcr <deibcr@gmail.com>
2024-10-23 11:40:53 -0400

    Verified Room.isAvailable

--- HotelOperations/src/main/java/com/pluralsight/hotelOperations/HotelApp.java
+++ HotelOperations/src/main/java/com/pluralsight/hotelOperations/HotelApp.java
@@ -4,7 +4,9 @@ public class HotelApp {
     public static void main(String[] args) {
         Room r= new Room(1,99.99);
 
-        System.out.println(r);
+        ///System.out.println(r);
+        if (r.isAvailable())
+        System.out.println("The Room is  available");
     }
 
 }
